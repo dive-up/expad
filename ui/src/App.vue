a4714fbe3029d5ee6cebfa1e7432004f90cb9cb1
Resham Singh Lotay <63938269+dive-up@users.noreply.github.com>
2026-04-23 07:44:11 -0400

    Vue Setup

---
+++ ui/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Main />
+  <Footer />
+</template>
+
+<style scoped>
+* {
+  width: 100%;
+  margin: auto;
+  padding: 0;
+}
+
+.app {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
